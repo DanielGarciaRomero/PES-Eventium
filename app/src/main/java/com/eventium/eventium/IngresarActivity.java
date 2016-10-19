743b73e491c01d2cedc7ea1455e11beb1f30d2f3
Alvaro Martinez <alvaro.martinez.arroyo@est.fib.upc.edu>
2016-10-19 20:07:29 +0200

    IngresarSaldoActivity

--- app/src/main/java/com/eventium/eventium/IngresarActivity.java
+++ app/src/main/java/com/eventium/eventium/IngresarActivity.java
@@ -1,34 +1,30 @@
 package com.eventium.eventium;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.CalendarView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class CalendarioActivity extends AppCompatActivity {
+public class IngresarActivity extends AppCompatActivity implements View.OnClickListener{
 
-    CalendarView calendar;
     Toolbar mytoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendario);
+        setContentView(R.layout.activity_ingresar);
 
         mytoolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mytoolbar);
-        getSupportActionBar().setTitle(R.string.toolbar_calendario);
+        getSupportActionBar().setTitle(R.string.toolbar_saldo);
         mytoolbar.setNavigationIcon(R.drawable.ic_menu);
 
-        calendar = (CalendarView) findViewById(R.id.calendar);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int day){
-                Toast.makeText(getApplicationContext(), day + "/" + (month+1) + "/" + year, Toast.LENGTH_LONG).show();
-            }
-        });
+        Button realizar_ingreso = (Button) findViewById(R.id.button15);
+        realizar_ingreso.setOnClickListener(this);
     }
 
     @Override
@@ -37,5 +33,11 @@ public class CalendarioActivity extends AppCompatActivity {
             Toast.makeText(getBaseContext(), "Has clicado en el menu", Toast.LENGTH_LONG).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.button15) {
+            Toast.makeText(getBaseContext(), "Has pulsado relizar ingreso", Toast.LENGTH_LONG).show();
+        }
     }
 }
