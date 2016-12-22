92cc4eb1a71743bd28d2c8a16a8d04c1de1ff662
Dagaro <daniel.garcia.romero@est.fib.upc.edu>
2016-12-22 16:15:47 +0100

    Diseno comentarios II

--- app/src/main/java/com/eventium/eventium/MyDialogFragmentComments.java
+++ app/src/main/java/com/eventium/eventium/MyDialogFragmentComments.java
@@ -11,13 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MyDialogFragmentComments extends DialogFragment {
 
     ListView lv;
+    EditText comentario;
+    Button publicar;
     Context c;
     AdapterComments adapCom;
     ArrayList<String> usernames;
@@ -38,12 +43,24 @@ public class MyDialogFragmentComments extends DialogFragment {
         usernames = new ArrayList<String>();
         usernames.add("Alvaro");
         usernames.add("Daniel");
+        usernames.add("Alvaro");
+        usernames.add("Daniel");
+        usernames.add("Señor Rambal");
         comments = new ArrayList<String>();
-        comments.add("First comment!");
-        comments.add("Second comment ...");
+        comments.add("Dada la enorme propensión de los españoles a jugar a la lotería en comparación con otros países, ¿significa esto que somos una sociedad de" +
+                " esperanzados irracionales? Si es así, lo somos la gran mayoría, pues solo un 22% dice no participar en el sorteo de la lotería en Navidad ...");
+        comments.add("Compramos lotería por si acaso les toca a quienes nos rodean. Los anuncios de Lotería de Navidad explotan esa envidia sin pudor alguno," +
+                " poniendo el énfasis no tanto en los agraciados con el Gordo, sino en los desgraciados que, habiendo podido, no compraron el décimo ganador.");
+        comments.add("Yo no compro lotería de Navidad desde que los anuncios los dejó de protagonizar el calvo aquel.");
+        comments.add("Mucho mejor el de Raphael y la Caballé, o el de Antoñito y el sobre.");
+        comments.add("Ya que mencionáis al calvo: ¿sabéis que no era calvo? Se tenía que rapar cada año para grabar el anuncio. Firmó un acuerdo de exclusividad " +
+                "con Loterías y Apuestas del Estado que le impedía participar en cualquier otra producción audiovisual entre 1998 y 2006 a cambio de 48.000 euros al año.");
         BitmapDrawable drawable = (BitmapDrawable) ContextCompat.getDrawable(c, R.drawable.defaultuserimage);
         Bitmap bitmap = drawable.getBitmap();
         images = new ArrayList<Bitmap>();
+        images.add(bitmap);
+        images.add(bitmap);
+        images.add(bitmap);
         images.add(bitmap);
         images.add(bitmap);
         adapCom = new AdapterComments(c, usernames, comments, images);
@@ -52,6 +69,15 @@ public class MyDialogFragmentComments extends DialogFragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // ¿Que hacer si me clican en un comentario? En teoria, nada
+            }
+        });
+        comentario = (EditText) view.findViewById(R.id.editText_add_comment);
+        publicar = (Button) view.findViewById(R.id.botonPublicar);
+        publicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(c, "Has pulsado Publicar", Toast.LENGTH_LONG).show();
+                dismiss();
             }
         });
     }
